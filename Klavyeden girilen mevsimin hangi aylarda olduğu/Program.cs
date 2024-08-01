023a2191e2f7a437e3a35f8e4f766b03a8b0e44c
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:59:37 +0300

    Add project files.

---
+++ Klavyeden girilen mevsimin hangi aylarda olduğu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_girilen_mevsimin_hangi_aylarda_olduğu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mevsimi girin");
+            string mevsim = Console.ReadLine();
+
+            switch (mevsim)
+            {
+                case "kış": Console.WriteLine("Aralık, Ocak, Şubat"); break;
+                case "ilkbahar": Console.WriteLine("Mart, Nisan, Mayıs"); break;
+                case "yaz": Console.WriteLine("Haziran, Temmuz, Ağustos"); break;
+                case "sonbahar": Console.WriteLine("Eylül, Ekim, Kasım"); break;
+
+                default: Console.WriteLine("Böyle bir mevsim yok"); break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
